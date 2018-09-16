7f3f3d5ce3d2c7396ccdf6f308a61c8f573c819d
katongole <katsisaac50@gmail.com>
2018-09-16 19:05:55 +0200

    initial commit

---
+++ src/components/Resume.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Grid, Cell } from 'react-mdl'
+
+class Resume extends Component {
+  render () {
+    return (
+      <div>
+        <Grid>
+          <Cell col={4}>
+            Left Side
+          </Cell>
+          <Cell className='resume-right-col' col={8}>
+            Right Side
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
