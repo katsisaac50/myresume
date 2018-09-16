4c4776895ae8e5baf0527c76b3e46a127f9abf7e
katongole <katsisaac50@gmail.com>
2018-09-16 21:49:56 +0200

    education props implemented

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import { Grid, Cell } from 'react-mdl'
 class Education extends Component {
   render () {
     return (
-      <div>
-        <p>
-          Education
-        </p>
-      </div>
+      <Grid>
+        <Cell col={4}>
+          <p>
+            {this.props.startYear}-
+            {this.props.endYear}
+          </p>
+        </Cell>
+        <Cell col={4}>
+          <h4 style={{marginTop: '0px'}}>{this.props.schoolName}</h4>
+          <p>
+            {this.props.schoolDescription}
+          </p>
+        </Cell>
+      </Grid>
     )
   }
 }
