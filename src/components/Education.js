9a569d1175aac738a6693677fc014b398dfbd177
katongole <katsisaac50@gmail.com>
2018-09-16 19:33:00 +0200

    Component Education created

---
+++ src/components/Education.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+class Education extends Component {
+  render () {
+    return (
+      <div>
+        <p>
+          Education
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Education
