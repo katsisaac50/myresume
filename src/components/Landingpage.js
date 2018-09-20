6f58401192e396c41240415ec4aa21f0919e3dac
katongole <katsisaac50@gmail.com>
2018-09-20 16:31:05 +0200

    added my picture

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Grid, Cell } from 'react-mdl'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Isaac from'../assets/Isaac1.png';
 
 class Landingpage extends Component {
   render () {
@@ -8,7 +9,7 @@ class Landingpage extends Component {
       <div style={{width: '100%', margin: 'auto'}}>
         <Grid className='landing-grid'>
           <Cell col={12}>
-            <img src='https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png' alt='avater' className='avatar-img' />
+            <img src={Isaac} alt='avater' className='avatar-img' />
             <div className='banner-text'>
               <h1>Full Stack Web Developer</h1>
               <hr/>
@@ -16,7 +17,11 @@ class Landingpage extends Component {
                 HTML/CSS|JavaScrip|React|DataBase|NodeJs
               </p>
               <div className='social-links'>
+                <a href='https://www.linkedin.com/in/katongole-isaac-2b27b6170/' rel='noopener noreferrer' target='_blank'><i className='fab fa-linkedin' aria-hidden='true' /></a>
                 <a href='http://google.com' rel='noopener noreferrer' target='_blank'><i className='fab fa-google' aria-hidden='true' /></a>
+                <a href='https://www.facebook.com/' rel='noopener noreferrer' target='_blank'><i className='fab fa-facebook' aria-hidden='true' /></a>
+                <a href='https://www.freecodecamp.org/katsisaac50' rel='noopener noreferrer' target='_blank'><i className='fab fa-free-code-camp' aria-hidden='true' /></a>
+                <a href='https://github.com/katsisaac50' rel='noopener noreferrer' target='_blank'><i className='fab fa-github' aria-hidden='true' /></a>
               </div>
             </div>
           </Cell>
