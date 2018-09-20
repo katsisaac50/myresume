715ecc577177dd53a3f1ee472654e805432fcff8
katongole <katsisaac50@gmail.com>
2018-09-20 16:28:43 +0200

    Navigaion added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
 import './App.css'
-import Resume from './components/Resume'
+import { Link } from 'react-router-dom'
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import Main from './components/Main'
+
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <Resume/>
+        <div className='demo-big-content'>
+          <Layout>
+            <Header className='header-color' title='My Resume' scroll>
+              <Navigation>
+              <Link to='/myresume'> Home
+                </Link>
+                <Link to='/resume'> Resume
+                </Link>
+                <Link to='/projects'> Projects
+                </Link>
+                <Link to='/aboutme'> About Me
+                </Link>
+              </Navigation>
+            </Header>
+            <Drawer title='My Resume'>
+            <Navigation>
+            <Link to='/myresume'> Home
+              </Link>
+              <Link to='/resume'> Resume
+              </Link>
+              <Link to='/projects'> Projects
+              </Link>
+              <Link to='/aboutme'> About Me
+              </Link>
+            </Navigation>
+            </Drawer>
+            <Content>
+              <div className='page-content' />
+              <Main/>
+            </Content>
+          </Layout>
+        </div>
       </div>
     )
   }
