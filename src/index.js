49ba4f2a84925ba49bc69455f95a91ab202dea1a
katongole <katsisaac50@gmail.com>
2018-09-20 16:29:40 +0200

    fontAwesome and BrowseRouter added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import 'react-mdl/extra/material.css'
 import 'react-mdl/extra/material.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+/* library.add(faStroopwafel) */
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('root'))
 registerServiceWorker()
