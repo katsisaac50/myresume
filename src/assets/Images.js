36d9bb7af461f3dcfc3eb95c8c9e97cd48b81e13
katongole <katsisaac50@gmail.com>
2019-12-24 02:05:25 +0100

    modified

---
+++ src/assets/Images.js
@@ -0,0 +1,6 @@
+export { default as Img01 } from './img-01.png';
+export { default as Img02 } from './img-02.jpg';
+export { default as Img03 } from './img-03.svg';
+/* export { default as Img01 } from './img-01.png'
+export { default as Img02 } from './img-02.jpg'
+export { default as Img03 } from './img-03.svg' */
